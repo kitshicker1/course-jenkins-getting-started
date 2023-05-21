71cc1717917d1ad3a1cb4f13f0dd9e8182efcb25
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2023-05-21 11:47:50 -0500

    "initial email that is sent on every build"

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -17,8 +17,16 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+                //}
+                //changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input",
+                        body: "Please go to ${BUILD_URL} and verify the build",
+                        attachLog: true, 
+                        compressLog: true, 
+                        to: "test@jenkins",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
+                        
                 }
-                
             }
         }
     }
