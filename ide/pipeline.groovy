a32fa72657c5dce70d714788b955e80aebab3d25
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2023-05-21 11:56:51 -0500

    "refactor back to the actual build process with notifications on changed build status only"

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -17,8 +17,8 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                //}
-                //changed {
+                }
+                changed {
                     emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input",
                         body: "Please go to ${BUILD_URL} and verify the build",
                         attachLog: true, 
