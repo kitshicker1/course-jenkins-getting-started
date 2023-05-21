82f70ce613be7dd2dcc5ccd0f42b7c2bdeddae21
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2023-05-20 21:59:02 -0500

    add triggers

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/kitshicker1/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
@@ -18,14 +18,7 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
-                }
+                
             }
         }
     }
