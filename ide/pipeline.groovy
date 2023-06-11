704fa41f1c3fab85b8e9b3f23962d3a9b5316aef
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2023-06-11 16:25:56 -0500

    main

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/kitshicker1/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/kitshicker1/jgsu-spring-petclinic.git', branch: 'master'
             }            
         }
         stage('Build') {
